b5994532b166badad0a3a01e90afc63e5d1187bd
kinghai <kinghai@kinghaimatoiMac.local>
2015-03-09 17:23:09 +0800

    submit first version

--- iosdemo/ViewController.swift
+++ iosdemo/ViewController.swift
@@ -7,9 +7,19 @@
 //
 
 import UIKit
+import CoreMedia
+import MediaPlayer
+
+import AVKit
+import AVFoundation
+import OnecloudVideo
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var urlField: UITextField!
+  
+    @IBOutlet weak var refererField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +30,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func playBtnClick(sender: AnyObject) {
+        playVideo()
+    }
+    
+    func playVideo() {
+        
+        let url = urlField.text
+        let referer = refererField.text
+        
+        if let player = OnecloudVideoUtil.getAVPlayer(url, referer: referer) {
+            
+            let playerController = AVPlayerViewController()
+            
+            playerController.player = player
+            
+            self.addChildViewController(playerController)
+            self.view.addSubview(playerController.view!)
+            
+            playerController.view.frame = self.view.frame
+            
+            player.play()
+            
+        }
+        else {
+            NSLog("no player")
+        }
+    }
 }
-
